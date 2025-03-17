7ebd8c4efcdb06a62894fdbc116d0bbbe9bf9742
Wisley César <wisleycesar32@gmail.com>
2025-03-17 20:49:44 -0300

    Atividade 02 de mineração de texto

---
+++ Atividade-02/atividade02.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Ler o arquivo original
+df = pd.read_csv('C1-Data Collection/C1-L1-Collect Data From Files-Data/ds_salaries.csv')
+
+# Exportar o arquivo com os requisitos
+df.to_csv("ds_trans_salario.csv", sep=';', decimal=',', encoding='utf-8', index=False)
+
+print("Arquivo exportado com sucesso!")
